77d17aa499ea83991b19413be6c395194b9574c1
Kalugin Dmitrii <kdm@one-touch.ru>
2023-09-01 16:47:26 +0400

    zapushil

--- lexicon/src/App.js
+++ lexicon/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import './App.css'
+import words from './words/words-2000.json'
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+  const [score, setScore] = useState(0)
+
+  const handleAnswerButtonClick = (points) => {
+    setScore(score + points)
+
+    const nextQuestion = currentQuestion + 1
+    if (nextQuestion < words.length) {
+      setCurrentQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container main-block ">
+        <Row>
+          <Col>
+            <div className="score-section d-flex justify-content-center">
+              Вы набрали {score} из {words.length} баллов!
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <div className="question-section d-flex justify-content-center flex-column align-items-center">
+            <div className="question-count">
+              <span>Вопрос {currentQuestion + 1}</span>/{words.length}
+            </div>
+            <div>
+              <h3>знаете это слово?</h3>
+            </div>
+            <div className="question-text">
+              <h2>{words[currentQuestion]}</h2>
+            </div>
+          </div>
+        </Row>
+        <Row>
+          <Col className="d-grid gap-3 col-2 mx-auto">
+            <Button
+              onClick={() => handleAnswerButtonClick(1)}
+              variant="primary"
+              size="lg"
+            >
+              know
+            </Button>
+            <Button onClick={() => handleAnswerButtonClick(32)}>
+              {' '}
+              hear somwhere
+            </Button>
+            <Button onClick={() => handleAnswerButtonClick(35)}>
+              {' '}
+              need context
+            </Button>
+            <Button onClick={() => handleAnswerButtonClick(43)}>
+              {' '}
+              не знаю
+            </Button>
+          </Col>
+        </Row>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
