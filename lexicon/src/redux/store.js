866a777f20d0d12fa5994af804d46eb64b869cc1
todayev <segodnyaev@gmail.com>
2023-09-18 09:29:09 +0300

    add persistent store

--- lexicon/src/redux/store.js
+++ lexicon/src/redux/store.js
@@ -1,7 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
 import lookSlice from './slices/lookSlice'
 import scoreSlice from './slices/scoreSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, combineReducers({
+  lookSlice, scoreSlice 
+}))
+
 export const store = configureStore({
-  reducer: { lookSlice, scoreSlice },
+  reducer: persistedReducer,
 })
+
+export const persistor = persistStore(store)
